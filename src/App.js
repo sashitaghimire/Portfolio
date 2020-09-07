17ebb1b87f7c1305592cb82bd982a1a95fb76755
sashitaghimire <ghimiresashita@gmail.com>
2020-09-07 22:17:13 +0545

    social_icons added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
 
 class App extends Component{
     state ={displayBio: false};
@@ -28,6 +29,9 @@ class App extends Component{
         }
         <hr />
         <Projects />
+        <hr />
+
+        <SocialProfiles />
             </div>
         )
     }
